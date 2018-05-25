4021af502476ea75c5640e9e691b119b5cb13296
Chang Gyo Jung <cgjung86@nau.edu>
2018-05-25 10:45:03 -0700

    Add files via upload

--- data_store/views.py
+++ data_store/views.py
@@ -5,31 +5,26 @@ from rest_framework.views import APIView
 from rest_framework.reverse import reverse
 from pymongo import MongoClient
 from api import config
-from .models import dumb_model
+from .models import dataStore
 # Create your views here.
 from rest_framework.settings import api_settings
-from .mongo_paginator import MongoDataPagination, MongoDistinct, MongoDataGet
+from .mongo_paginator import MongoDataPagination, MongoDistinct,MongoGroupby, MongoDataGet,MongoDataDelete,MongoDataSave,MongoDataInsert
 from .renderer import DataBrowsableAPIRenderer, mongoJSONPRenderer,mongoJSONRenderer
 from rest_framework.renderers import XMLRenderer, YAMLRenderer,JSONPRenderer
 from rest_framework.parsers import JSONParser
-
+from permission import  DataStorePermission, createDataStorePermission
+ 
 class MongoDataStore(APIView):
-    permission_classes = ( IsAuthenticatedOrReadOnly,)
+    permission_classes = ( createDataStorePermission,)
     renderer_classes = (DataBrowsableAPIRenderer, mongoJSONRenderer, mongoJSONPRenderer, XMLRenderer, YAMLRenderer)
     title = "Database"
     parser_classes = (JSONParser,)
     connect_uri = config.DATA_STORE_MONGO_URI
     view_reverse='data'
-    name = "Grundge"
+    name = "exclude"
     exclude= config.DATA_STORE_EXCLUDE
-    #db=MongoClient(host=self.connect_uri)
     def __init__(self):
         self.db = MongoClient(host=self.connect_uri)
-    #def dispatch(self, request, *args, **kwargs):
-        #(self, connect_uri=config.DATA_STORE_MONGO_URI):
-     #   print self.connect_uri
-      #  self.db = MongoClient(host=self.connect_uri)
-      #  return super(MongoDataStore, self).dispatch(request, *args, **kwargs)
     def get(self, request, database=None, format=None):
         #self.db = MongoClient(host=self.connect_uri)
         urls = []
@@ -48,6 +43,9 @@ class MongoDataStore(APIView):
             self.title = "Database"
             data = list(self.db.database_names())
             data.sort()
+            #This section used for catalog django app
+            if self.name == "include":
+                data = self.include
 
             for db in data:
                 if db in self.exclude:
@@ -56,21 +54,70 @@ class MongoDataStore(APIView):
                     urls.append(reverse("%s-list"% (self.view_reverse), kwargs={'database': db}, request=request))
             return Response({
                 'Available Databases': urls})
+    def post(self,request,database=None,format=None):
+            #Action Delete
+            action=request.DATA.get('action', '')
+            collection=request.DATA.get('collection', None)
+            
+            if action.lower()=='delete':
+                #print(config.FORCE_SCRIPT_NAME)
+                try:
+                    shift_url = len(config.FORCE_SCRIPT_NAME.split('/'))
+                    #print shift_url
+                    if shift_url>1:
+                        shift=shift_url -3
+                    else:
+                        shift=-1
+                except:
+                    shift =-1
+                if collection:
+                    if len(request.path.split('/'))==6+shift:
+                        try:
+                            self.db[database].drop_collection(collection)
+                            return Response({collection:"Deleted"})
+                        except Exception as e:
+                            return Response({"Error":str(e)})
+                    else:
+                        return Response({"ERROR":"Must be on Collection View to drop collection."})
+                elif not database:
+                    if len(request.path.split('/'))==5+shift:
+                        database=request.DATA.get('database', None)
+                        try:
+                            self.db.drop_database(database)
+                            return Response({database:"Deleted"})
+                        except Exception as e:
+                            return Response({"Error":str(e)})
+                    else:
+                        return Response({"ERROR":"Must be on Database View to drop database."})
+                else:
+                    return Response({"ERROR":"Database {0} Collection {1} Action {2}".format(database,collection,action)})
+            #Action Create (default None)
+            if database:
+                col=request.DATA.get('collection', None)
+                if col:
+                    data = request.DATA.get('data', {})
+                    self.db[database][col].insert_one(data)
+                    self.db[database][col].remove({})
+                    return Response({'database':database,'collection':col})
+                else:
+                    return Response({'ERROR':"Must submit 'collection' name as part of post"})
+            else:
+                data = request.DATA.get('database', None)
+                if data:
+                    self.db[data]['default_collection'].insert_one({})
+                    return Response({'database':data})
+                else:
+                    return Response({'ERROR':"Must submit 'database' name as part of post"})
 
 
 class DataStore(APIView):
-    permission_classes = (DjangoModelPermissionsOrAnonReadOnly,)
-    model = dumb_model
+    permission_classes = (DataStorePermission,) #DjangoModelPermissionsOrAnonReadOnly,)
+    model = dataStore 
     renderer_classes = (DataBrowsableAPIRenderer, mongoJSONRenderer, mongoJSONPRenderer, XMLRenderer, YAMLRenderer)
     parser_classes = (JSONParser,)
     connect_uri = config.DATA_STORE_MONGO_URI
     def __init__(self):
         self.db = MongoClient(host=self.connect_uri)
-    #def dispatch(self, request, *args, **kwargs):
-        #(self, connect_uri=config.DATA_STORE_MONGO_URI):
-    #    print self.connect_uri
-    #    self.db = MongoClient(host=self.connect_uri)
-    #    return super(DataStore, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, database=None, collection=None, format=None):
         #self.db = MongoClient(host=self.connect_uri)
@@ -95,14 +142,26 @@ class DataStore(APIView):
                 data = MongoDistinct(field,self.db, database, collection, query=query)
             else:
                 data = {"ERROR":"Must provide keyword field to perform distinct operation."}
+        elif action.lower()=="groupby":
+            variable = request.QUERY_PARAMS.get('variable',None)
+            if not variable:
+                data = {"ERROR":"Must provide keyword field to perform aggregation operation."}
+            groupby=request.QUERY_PARAMS.get('groupby',None)
+            if groupby:
+                gbs=groupby.split(',')
+                data = MongoGroupby(variable,gbs,self.db, database, collection, query=query)
+            else:
+                data = {"ERROR":"Must provide groupby column names. Multiple separate by comma."}
         else:
             data = MongoDataPagination(self.db, database, collection, query=query, page=page, nPerPage=page_size, uri=url)
         return Response(data)
     def post(self,request,database=None,collection=None,format=None):
-        return Response(request.DATA)
+        result =MongoDataInsert(self.db, database, collection,request.DATA)
+        return Response(result)
+
 class DataStoreDetail(APIView):
-    permission_classes = (DjangoModelPermissionsOrAnonReadOnly,)
-    model = dumb_model
+    permission_classes = (DataStorePermission,) #DjangoModelPermissionsOrAnonReadOnly,)
+    model = dataStore
     renderer_classes = (DataBrowsableAPIRenderer, mongoJSONRenderer, mongoJSONPRenderer, XMLRenderer, YAMLRenderer)
     parser_classes = (JSONParser,)
     connect_uri = config.DATA_STORE_MONGO_URI
@@ -111,28 +170,8 @@ class DataStoreDetail(APIView):
     def get(self,request,database=None, collection=None,id=None, format=None):
         data = MongoDataGet(self.db,database,collection,id)
         return Response(data)
-"""
-        if query:
-            query = ast.literal_eval(query)
-            print query
-            q = [ (k, v) for k, v in query['spec'].items() ]
-            query['spec'] = dict(q)
-            print query
-            data = [row for row in self.db[database][collection].find(**query)]
-        else:
-            data = [row for row in self.db[database][collection].find()]
-
-        paginator = Paginator(data,page_size)
-
-        try:
-            result =paginator.page(page)
-        except PageNotAnInteger:
-            result = paginator.page(1)
-        except EmptyPage:
-            result = paginator.page(paginator.num_pages)
-        serializer = PaginationSerializer(instance=result,context={'request': request})
-        return Response(serializer.data)
-    #def put(self,request,database=None,collection=None,format=None):
-    #    return Response(request.DATA)
-
-"""
+    def put(self,request,database=None, collection=None,id=None, format=None):
+        return Response(MongoDataSave(self.db,database,collection,id,request.DATA))
+    def delete(self,request,database=None, collection=None,id=None, format=None):
+        result = MongoDataDelete(self.db,database,collection,id)
+        return Response({"deleted_count":result.deleted_count,"_id":id})
